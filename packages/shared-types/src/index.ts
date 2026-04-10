54d34eccad4677a0b069c1f1dd752f42ad2f0772
sarbajit <sarbajit.pal@games24x7.com>
2026-04-10 16:40:00 +0530

    Initialize apnapal monorepo

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,11 @@
+export interface ApiResponse<T> {
+  data: T;
+  message?: string;
+}
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  total: number;
+  page: number;
+  pageSize: number;
+}
